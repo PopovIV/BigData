7803b8cfe9416b5f97685051e67ac89c58ff76b7
PopovIV <71888203+PopovIV@users.noreply.github.com>
2022-11-18 03:49:08 +0300

    BIG fix for lab 6

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -1,67 +1,52 @@
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-from scipy import stats as stats
-import seaborn as sns
+from scipy import stats
 
-N = 10000
-n = 100
-k = 1.44
-
-def generateNormal():
-    return stats.norm.rvs(size = n)
-
-def generateCauchy():
-    return stats.cauchy.rvs(size = n)
-
-def generateMix():
-    return 0.9 * stats.norm.rvs(size = n) + 0.1 * stats.cauchy.rvs(size = n)
-
-def huber(x):
+def huber(x: np.array, k: float):
     phi = lambda elem: elem if abs(elem) < k else k * np.sign(elem)
+
     return np.mean([phi(elem) for elem in x])
 
-def twoFak(x):
-    r = plt.boxplot(x)
-    topPoints = r["fliers"][0].get_data()[1]
-    b = np.delete(x, np.array([(x[i] in topPoints) for i in range(x.size)]))
-    return b.mean()
+def sigm3_rull(x: np.array):
 
-def monteKarlo(sample, func):
-    means = [func(sample) for _ in range(N)]
+    xmean = np.mean(x)
+    xvar = np.var(x)
+    is_in_3sigm = lambda xi: np.abs(xi - xmean) < 3*xvar
+
+    clear_res = [val for val in x if is_in_3sigm(val)]
+
+    return clear_res
+
+def double_stage_mean(x: np.array):
+    x = sigm3_rull(x)
+    return np.mean(x)
+
+distributions = {
+    "norm": stats.norm.rvs,
+    "cauchy": stats.cauchy.rvs,
+    "mix": lambda size: 0.9 * stats.norm.rvs(size=size) + 0.1 * stats.cauchy.rvs(size=size) 
+}
+
+measures = {
+    "mean": np.mean,
+    "median": np.median,
+    "huber": lambda x: huber(x, 1.44),
+    "two_fac": double_stage_mean
+}
+
+def monte_karlo(N: int, sample_size: int, dist_grvs, measure):
+    means = [measure(dist_grvs(size=sample_size)) for _ in range(N)]
+
     return np.mean(means), np.var(means)
 
+def main(N : int = 10000, sample_size: int = 100):
+    for dname, grvs in distributions.items():
+        print(dname)
+        for mname, measure in measures.items():
+            mu, var = monte_karlo(N, sample_size, grvs, measure)
+            print(f"\t{mname}")
+            print(f"\t\tmu:\t{mu:.6f}")
+            print(f"\t\tvar:\t{var:.6f}")
+        print("")
+
 if __name__ == "__main__":
-    print("N(0, 1):\n")
-    sample = generateNormal()
-    mean, var = monteKarlo(sample, np.mean)
-    print("Mean: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, np.median)
-    print("Median: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, huber)
-    print("Huber: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, twoFak)
-    print("2 fak: mean: " + str(mean) + " , var: " + str(var) + "\n")
-
-    print("C(0, 1):\n")
-    sample = generateCauchy()
-    mean, var = monteKarlo(sample, np.mean)
-    print("Mean: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, np.median)
-    print("Median: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, huber)
-    print("Huber: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, twoFak)
-    print("2 fak: mean: " + str(mean) + " , var: " + str(var) + "\n")
-
-    print("0.9 * N(0,1) + 0.1 * C(0, 1):\n")
-    sample = generateCauchy()
-    mean, var = monteKarlo(sample, np.mean)
-    print("Mean: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, np.median)
-    print("Median: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, huber)
-    print("Huber: mean: " + str(mean) + " , var: " + str(var) + "\n")
-    mean, var = monteKarlo(sample, twoFak)
-    print("2 fak: mean: " + str(mean) + " , var: " + str(var) + "\n")
-
+    main()
